88eba4a06ea8f0577070938bd5d441de17a9c25d
PrakruthiPrasad <bagursuryanarayana.p@northeastern.edu>
2021-12-01 19:41:40 -0500

    fixed null pointer exception

--- src/main/java/com/neu/lambda/UserEvent.java
+++ src/main/java/com/neu/lambda/UserEvent.java
@@ -23,36 +23,40 @@ import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 public class UserEvent implements RequestHandler<SNSEvent, Object> {
 	
 	static final String FROM = "Webapp@prod.csye6225dnsbagur.me";
-    static final String SUBJECT = "Email Verification";
+    static final String SUBJECT = "Email Verification for Cloud Native Web Application - CSYE6225";
     String HTMLBODY = "";
     static String TEXTBODY = "";
 
     public Object handleRequest(SNSEvent request, Context context){
 
-        try {
+    	try {
+            String HTMLBODY = "";
+            String TEXTBODY = "";
             AmazonDynamoDB dynamoclient = AmazonDynamoDBClientBuilder.standard().build();
             String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
             context.getLogger().log("Invocation started: " + timeStamp);
             String msg = request.getRecords().get(0).getSNS().getMessage();
-            String split_msg[] = msg.split(":");
+            System.out.println("Msg recieved: "+msg);
+            String split_msg[] = msg.split("::");
             String To_Email = split_msg[0];
             String token = split_msg[1];
             String ttl = split_msg[2];
+            System.out.println("to email: "+split_msg[0]);
+            System.out.println("token "+split_msg[1]);
             String verify_url = "http://prod.csye6225dnsbagur.me/v1/user/EmailVerification?email="+To_Email+"&token="+token+"&ttl="+ttl;
             HTMLBODY+="<h1>Click on below Link to Verify your email address:</h1>";
             HTMLBODY+= new URL(verify_url);
             HTMLBODY+="<br>";
             GetItemRequest req = new GetItemRequest();
-            req.setTableName("MailSent");
+            req.setTableName("csye6225_Email");
             req.setConsistentRead(true);
             Map<String, AttributeValue> keysMap = new HashMap();
-            keysMap = req.getKey();
-            Boolean mailsent_flag = false;
-            for (AttributeValue value : keysMap.values()) {
-               if (value.equals(To_Email))
-                { mailsent_flag = true;}
-            }
-            if(mailsent_flag==false) {
+            keysMap.put("username",new AttributeValue(To_Email));
+            req.setKey(keysMap);
+            GetItemResult result = dynamoclient.getItem(req);
+            if (result.getItem() != null)
+                {  System.out.println("Email already sent to: "+To_Email);}
+            else {
                 AmazonSimpleEmailService client =  AmazonSimpleEmailServiceClientBuilder.standard().build();
                 SendEmailRequest email_req = new SendEmailRequest()
                         .withDestination(
@@ -67,23 +71,24 @@ public class UserEvent implements RequestHandler<SNSEvent, Object> {
                                         .withCharset("UTF-8").withData(SUBJECT)))
                         .withSource(FROM);
                 client.sendEmail(email_req);
-                System.out.println("Email sent!");
+                System.out.println("Email sent!"+ To_Email);
                 context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
                 Map<String, AttributeValue> map = new HashMap();
-                map.put(To_Email, new AttributeValue(To_Email));
+                map.put("username", new AttributeValue(To_Email));
                 PutItemRequest request11 = new PutItemRequest();
-                request11.setTableName("MailSent");
+                request11.setTableName("csye6225_Email");
                 request11.setItem(map);
                 PutItemResult result1 = dynamoclient.putItem(request11);
             }
             timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
             context.getLogger().log("Invocation completed: " + timeStamp);
         }catch(Exception e) {
-                System.out.println("Email not sent");
-                System.out.println( e.getMessage());
-                System.out.println("Stack trace: ");
-                e.printStackTrace();
-            }
+            System.out.println("Email not sent");
+            System.out.println( e.getMessage());
+            System.out.println("Stack trace: ");
+            e.printStackTrace();
+        }
+     
         return null;
 
 
